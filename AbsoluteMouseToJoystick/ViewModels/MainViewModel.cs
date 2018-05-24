48fc860ddb91d83df5f6a45bcd351dd2f914f669
Unknown <swisrud@gmail.com>
2018-05-24 20:39:58 +0200

    Cleanup TODO list

--- AbsoluteMouseToJoystick/ViewModels/MainViewModel.cs
+++ AbsoluteMouseToJoystick/ViewModels/MainViewModel.cs
@@ -20,14 +20,9 @@ namespace AbsoluteMouseToJoystick.ViewModels
     /* TODO:
      * - Show current values in app (button)
      *  - Also show dot on preview
-     * - Add some kind of MVVM
-     *  - Split into regions (region for axisX region for axisX preview etc)
-     *  - Make them reusable (region for axisX and Y should only differ in a parameter passed
-     * - Save options to files (json)
      * - Add textbox for device ID
      * - Allow for use of different axes
      * - Add tooltips over preview, buttons and fields
-     * - Allow creation of profiles to save preset for different conditions or games
      */
 
     public class MainViewModel : ViewModelBase
